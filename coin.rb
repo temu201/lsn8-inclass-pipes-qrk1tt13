36964bf0becf848393a60cbb3b164e8d98f6e429
temu201 <119833918+temu201@users.noreply.github.com>
2022-11-17 11:41:05 +0000

    fint

--- coin.rb
+++ coin.rb
@@ -13,4 +13,4 @@ end
 # Write a function that takes a number of cents, and that returns a hash containing the least number of coins needed to generate the value of cents.
 
 # eg. If you have 11 cents you should return {:dime => 1, :penny => 1};
-# eg. If you have 31 cents you should return {:quarter => 1, :nickel => 1, :penny => 1
+# eg. If you have 31 cents you should return {:quarter => 1, :nickel => 1, :penny => 1}
