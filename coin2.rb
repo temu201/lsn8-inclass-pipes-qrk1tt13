6ec808dd260ad40fac6390aa7142ebc5dc747e8f
temu201 <119833918+temu201@users.noreply.github.com>
2022-12-31 18:08:11 +0000

    java8

--- coin2.rb
+++ coin2.rb
@@ -3,3 +3,4 @@ def coin_value(amount)
 	cents = {:quarter => 25, :dime => 10, :nickle => 5, :penny => 1}
 
 	change = {:quarter => 0, :dime => 0, :nickle => 0, :penny => 0}
+	cents.each do |key, value|
